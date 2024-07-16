4ece3b410118a85d0b3da607dcf16b7d1b3f32d8
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-07-16 11:40:05 +0100

    I restructured booking-app-form.tsx I incoporated a library for a better user interface

--- components/booking-app/booking-app-form.tsx
+++ components/booking-app/booking-app-form.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import { TbAlertCircle } from "react-icons/tb";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const lawyers = [
   { id: "1", value: "lawyer1", label: "John Doe" },
@@ -10,29 +15,40 @@ const lawyers = [
   { id: "6", value: "lawyer6", label: "Joachim Sourh" },
 ];
 
-const BookingAppForm = () => {
-  const [clientData, setClientData] = useState({
-    name: "",
-    email: "",
-    phoneNo: "",
-  });
+const formSchema = z.object({
+  name: z.string().nonempty("Name is required").trim(),
+  email: z.string().nonempty("Email is required").email("Invalid email address"),
+  phoneNo: z.string().nonempty("Phone number is required").trim(),
+  date: z.string().nonempty("Date is required").trim(),
+  time: z.string().nonempty("Time is required").trim(),
+  lawyer: z.string().nonempty("Lawyer is required").trim(),
+  notes: z.string().nonempty("Notes is required").trim(),
+  payment: z.string().nonempty("Payment is required").trim(),
+});
 
+function BookingAppForm(): React.JSX.Element {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const [appointmentData, setAppointmentData] = useState({
-    date: "",
-    time: "",
     lawyer: null,
     payment: "",
-    notes: "",
   });
 
-  const handleClientChange = (event: any) => {
-    setClientData({
-      ...clientData,
-      [event.target.name]: event.target.value,
-    });
-  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      phoneNo: "",
+      date: "",
+      time: "",
+      lawyer: "",
+      notes: "",
+      payment: "",
+    },
+  });
 
-  const handleAppointmentChange = (event: any) => {
+  const handleAppointmentChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setAppointmentData({
       ...appointmentData,
       [event.target.name]: event.target.value,
@@ -46,137 +62,181 @@ const BookingAppForm = () => {
     });
   };
 
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setError("");
+    setLoading(true);
+    console.log(values);
+  }
+
   return (
-      <div className="w-[50%] p-4 bg-white">
-        <h1 className="text-3xl font-extrabold text-center text-primary-blue48 font-lilita">Book a Consultation and Take the First Step Toward Justice.</h1>
-
-        <form className="w-[90%] flex flex-col items-center">
-          <p className="text-center text-gray-700 italic pt-4">Client Information</p>
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">First Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={clientData.name}
-              onChange={handleClientChange}
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              placeholder="Input Your First Name"
-              required
-            />
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-5 max-w-md sm:max-w-md py-10 w-full sm:w-3/5 px-6 rounded-lg mx-auto fade-in-30">
+        <div>
+          <h2 className="text-3xl text-primary-blue48 font-lilita mt-20">Book a Consultation and Take the First Step Toward Justice.</h2>
+        </div>
+        {!!error && (
+          <div className="relative w-full rounded-lg border px-4 py-3 text-sm border-destructive/50 text-destructive flex items-center gap-2">
+            <TbAlertCircle className="size-4" />
+            <span className="text-sm">{error}</span>
           </div>
+        )}
 
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">Email Address:</label>
-            <input
-              type="email"
-              name="email"
-              value={clientData.email}
-              onChange={handleClientChange}
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              placeholder="Input Your Email Address"
-              required
-            />
-          </div>
-
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">Phone No:</label>
-            <input
-              type="text"
-              name="phoneNo"
-              value={clientData.phoneNo}
-              onChange={handleClientChange}
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              placeholder="Input Your Phone Number"
-              required
-            />
-          </div>
-
-          <p className="text-center text-gray-800 italic pt-2">Appointment Details</p>
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">Date</label>
-            <input
-              type="date"
-              name="date"
-              value={appointmentData.date}
-              onChange={handleAppointmentChange}
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              placeholder="Select a preferred date"
-              required
-            />
-          </div>
-
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">Time:</label>
-            <input
-              type="time"
-              name="time"
-              value={appointmentData.time}
-              onChange={handleAppointmentChange}
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              placeholder="Select a preferred time"
-              required
-            />
-          </div>
-
-          <div className="mb-4 w-[90%]">
-            <label className="block mb-2 font-extrabold text-gray-900">Choose a Lawyer</label>
-            <Select
-              name="lawyer"
-              options={lawyers}
-              value={appointmentData.lawyer}
-              onChange={handleLawyerChange}
-              className="w-full placeholder-slate-400 placeholder-opacity-90 italic border rounded border-solid border-black"
-              placeholder="Select a lawyer"
-            />
-          </div>
-
-          <p className="text-center text-gray-800 italic pt-2">Payment Details</p>
-          <div className="mb-4 pt-4 w-[90%]">
-            <label htmlFor="payment" className="text-gray-900 font-extrabold mb-2 block">
-              Choose a payment method
-            </label>
-            <select
-              id="payment"
-              name="payment"
-              className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
-              value={appointmentData.payment}
-              onChange={handleAppointmentChange}
-              required>
-              <option value="" disabled selected className="text-black">
-                Select a payment method
-              </option>
-              <option value="paypal" className="text-black">
-                PayPal
-              </option>
-              <option value="stripe" className="text-black">
-                Stripe
-              </option>
-              <option value="bank_transfer" className="text-black">
-                Bank Transfer
-              </option>
-            </select>
-          </div>
-
-          <p className="text-center text-gray-800 italic pt-4">
-            <label htmlFor="notes">Additional Notes</label>
-          </p>
-          <textarea
-            name="notes"
-            id="notes"
-            rows={5}
-            cols={40}
-            className="px-8 py-2 w-full border rounded border-solid border-gray-300 placeholder-slate-400 placeholder-opacity-90 italic"
-            placeholder="Add any specific details about your case"
-            value={appointmentData.notes}
-            onChange={handleAppointmentChange}
+        <p className="text-center text-gray-800 italic pt-2">Client Information</p>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">First Name:</FormLabel>
+              <FormControl>
+                <input
+                  placeholder="John Doe"
+                  {...field}
+                  className="w-full h-10 rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">Email Address:</FormLabel>
+              <FormControl>
+                <input
+                  placeholder="email@example.com"
+                  {...field}
+                  className="w-full h-10 rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phoneNo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">Phone No:</FormLabel>
+              <FormControl>
+                <input
+                  placeholder="Input your Phone Number"
+                  {...field}
+                  className="w-full h-10 rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">Date:</FormLabel>
+              <FormControl>
+                <input
+                  placeholder="Select a preferred date"
+                  {...field}
+                  className="w-full h-10 rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="time"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">Time:</FormLabel>
+              <FormControl>
+                <input
+                  placeholder="Select a preferred time"
+                  {...field}
+                  className="w-full h-10 rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="mb-4 w-[90%]">
+          <label className="block mb-2 font-extrabold text-gray-900">Choose a Lawyer</label>
+          <Select
+            name="lawyer"
+            options={lawyers}
+            value={appointmentData.lawyer}
+            onChange={handleLawyerChange}
+            className="w-full placeholder-slate-400 placeholder-opacity-90 italic border rounded border-solid border-black"
+            placeholder="Select a lawyer"
           />
+        </div>
 
-          <button type="submit" className="w-1/2 px-4 py-2 mt-5 text-white bg-primary-orange61 rounded hover:bg-primary-orange61">
-            Book Appointment
+        <p className="text-center text-gray-800 italic pt-2">Payment Details</p>
+        <div className="mb-4 pt-4 w-[90%]">
+          <label htmlFor="payment" className="text-gray-900 font-extrabold mb-2 block">
+            Choose a payment method
+          </label>
+          <select
+            id="payment"
+            name="payment"
+            className="w-full px-10 py-2 border rounded border-solid border-black placeholder-slate-400 placeholder-opacity-90 italic"
+            value={appointmentData.payment}
+            onChange={handleAppointmentChange}
+            required>
+            <option value="" disabled selected className="text-black">
+              Select a payment method
+            </option>
+            <option value="paypal" className="text-black">
+              PayPal
+            </option>
+            <option value="stripe" className="text-black">
+              Stripe
+            </option>
+            <option value="bank_transfer" className="text-black">
+              Bank Transfer
+            </option>
+          </select>
+        </div>
+
+        <p className="text-center text-gray-800 italic pt-2">Additional Notes</p>
+        <FormField
+          control={form.control}
+          name="notes"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary-dark32/80 font-normal">Additional Notes</FormLabel>
+              <FormControl>
+                <textarea
+                  placeholder="Add any specific details about your case"
+                  {...field}
+                  className="w-full h-32 py-2 resize-none rounded-md placeholder:text-black/50 placeholder:text-sm placeholder:font-normal px-5 text-primary-dark32 border border-primary-dark32/70 focus-visible:outline-none focus:outline-none"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex items-center justify-end gap-2 mt-4 mb-10">
+          <button
+            type="submit"
+            className="w-fit px-4 py-2 text-white text-sm bg-primary-orange61/85 rounded-md hover:bg-primary-orange61 duration-300">
+            {loading ? "Loading..." : "Book Appointment"}
+          
           </button>
-        </form>
-      </div>
+        </div>
+      </form>
+    </Form>
   );
-};
+}
 
 export default BookingAppForm;
