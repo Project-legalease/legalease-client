907edf08b176189aab920aa55dad064e418b70cc
chidexobikwelu <chidexobikwelu@gmail.com>
2024-07-17 14:12:22 +0000

    Designed the Payment Option Page. Not perfect for now, but will surely tidy things up later

---
+++ app/payment/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import { PaymentOptionsForm } from "@/components/payment/payment-option";
+
+export const metadata: Metadata = {
+  title: "Payment | LegalEase",
+  description: "Making legal access easy",
+};
+
+export default function Page() {
+  return <PaymentOptionsForm />;
+}
